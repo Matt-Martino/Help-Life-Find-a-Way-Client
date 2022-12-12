e43c6deb09998032c41849930476b88ce7ad8776
Matt <mattmartino54@gmail.com>
2022-12-12 16:00:23 -0600

    added ability to view all users, and their plant count (broken, please fix).  Allows user to then look into a selected users profile listing all their plants.

---
+++ src/components/user/allUsers.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { getAllUsers } from "../../managers/UserManager"
+
+
+
+export const ViewAllUsers = () => {
+    const [allUsers, setAllUsers] = useState([])
+    
+    useEffect(() => {
+        getAllUsers().then((userData) => setAllUsers(userData))
+    
+      }, [])
+
+
+
+      return (
+      <>
+        <h1 className="title is-1 level-item">All users</h1>
+        {
+          allUsers.map(user =>
+            { return <>
+                <section>
+
+                <div key={`user--${user.id}`}>
+                  <div className="level">
+                    <div className="columns level-item">
+                      <div className="card column is-three-quarters">
+                        <div className="card-image">
+                        </div>
+                        <div className="card-content">
+                          <div className="columns">
+                            <div className="column"> User {user.full_name}'s has {user.plant_count} plants in their collection.
+                              <div className="media">
+                              </div>
+                            </div>
+                            <div className="column">
+                              <div className="content">
+                                <div className="title is-3">
+                                  <Link to={`/plants/user/${user.id}`} > View {user?.user?.username} plant collection.
+                                  </Link>
+                                  
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                </section>
+           </> 
+           }
+          )
+        }
+      </>
+    )
+      
+}
