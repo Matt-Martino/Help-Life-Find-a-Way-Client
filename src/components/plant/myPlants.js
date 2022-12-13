adc65788d0289f3d56478c26139d8fda82dee883
Matt <mattmartino54@gmail.com>
2022-12-13 16:15:49 -0600

    approved MVP product

--- src/components/plant/myPlants.js
+++ src/components/plant/myPlants.js
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react"
-import { Link, Navigate, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { deletePlant, getCurrentLoggedInUsersPlants } from "../../managers/PlantManager"
 
 
 
 export const ViewMyPlants = () => {
-  const navigate = useNavigate()
   const [plants, setPlants] = useState([])
 
   useEffect(() => {
@@ -31,47 +30,43 @@ export const ViewMyPlants = () => {
 
   const makeDeleteRequest = (plantId) => {
     deletePlant(plantId).then(() => {
-      window.location.reload(false)})
+      window.location.reload(false)
+    })
   }
 
 
   const renderListOfUserPlants = () => {
-    return (
-      <>
-        <h1 className="title is-1 level-item">My Plants</h1>
-        {
-          plants.map(plant =>
-            plants.length > 0
-              ? <>
-                <div key={`plant--${plant.id}`}>
-                  <div className="level">
-                    <div className="columns level-item">
-                      <div className="card column is-three-quarters">
-                        <div className="card-image">
-                        </div>
-                        <div className="card-content">
-                          <div className="columns">
-                            <div className="column">
-                              <div className="media">
-                                <div className="media-left">
-                                <img src={plant.plant_image} className="uploaded_image" />
-                                </div>
-                                <div className="media-content">
-                                  <div>
-                                    <p className="title is-4"> {plant.user?.full_name}</p>
-                                  </div>
-                                </div>
+    return <>
+      {
+        plants.map(plant =>
+          <section key={`plants--${plant.id}`}>
+            <div >
+              <div className="level">
+                <div className="columns level-item">
+                  <div className="card column is-three-quarters">
+                    <div className="card-image">
+                    </div>
+                    <div className="card-content">
+                      <div className="columns">
+                        <div className="column">
+                          <div className="media">
+                            <div className="media-left">
+                              <img src={plant.plant_image} className="uploaded_image" key={`plants--${plant.id}`} />
+                            </div>
+                            <div className="media-content">
+                              <div>
+                                <p key={`plants--${plant.id}`} className="title is-4"> {plant.user?.full_name}</p>
                               </div>
                             </div>
-                            <div className="column">
-                              <div className="content">
-                                <div className="title is-3">
-                                  <Link to={`/plants/${plant.id}`}>
-                                    {plant.plant_name}.
-                                  </Link>
-                                  {deletePlantButton(plant.id)}
-                                </div>
-                              </div>
+                          </div>
+                        </div>
+                        <div className="column">
+                          <div className="content">
+                            <div className="title is-3">
+                              <Link to={`/plants/${plant.id}`}>
+                                {plant.plant_name}.
+                              </Link>
+                              {deletePlantButton(plant.id)}
                             </div>
                           </div>
                         </div>
@@ -79,20 +74,22 @@ export const ViewMyPlants = () => {
                     </div>
                   </div>
                 </div>
-                <br></br>
-                <br></br>
-                <br></br>
-                <br></br>
-              </>
-              : <> <h1 className="title">You have currently have No Plants.</h1>
-              </>
-          )
-        }
-      </>
-    )
+              </div>
+            </div>
+            </section>
+          
+
+        )
+      }
+    </>
   }
 
   return <>
-    {renderListOfUserPlants()}
+    <h1 className="title is-1 level-item">My Plants</h1>
+    {
+      (plants.length > 0)
+        ? renderListOfUserPlants()
+        : <h2> Add some plants to your collection.</h2>
+    }
   </>
 }
